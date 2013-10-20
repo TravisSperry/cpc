54495175b8dc22d95bd658d3701e77e8f49dbe9b
tkendalls <director@mathplusacademy.com>
2013-10-20 16:55:42 -0400

    commit

--- app/mailers/notifications_mailer.rb
+++
@@ -1,9 +0,0 @@
-class NotificationsMailer < ActionMailer::Base
-  default :from => "contact@columbuspowdercoat.com"
-  default :to => "travis@columbuspowdercoat.com"
-
-  def new_message(message)
-    @message = message
-    mail(:subject => "[columbuspowdercoat.com] #{message.subject}", :body => "From: #{message.email}- #{message.body}")
-  end
-end
