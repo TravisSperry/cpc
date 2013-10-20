59dcc8bb4d478d24352137473a003f7ed3f5039f
tkendalls <director@mathplusacademy.com>
2013-10-20 16:47:54 -0400

    updated spell errors

--- app/mailers/notification_mailer.rb
+++
@@ -1,9 +0,0 @@
-class NotificationMailer < ActionMailer::Base
-  default :from => "contact@columbuspowdercoat.com"
-  default :to => "travis@columbuspowdercoat.com"
-
-  def new_message(message)
-    @message = message
-    mail(subject: "[columbuspowdercoat.com] #{message.subject}")
-  end
-end
