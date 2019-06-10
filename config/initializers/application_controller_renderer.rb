690db380633192b366b2dee78def3eb55a4c45ac
Travis Sperry <travissperry1@gmail.com>
2019-06-09 22:36:31 -0400

    update

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,4 +1,4 @@
-# Be sure to restart your server when you modify this file.
+l# Be sure to restart your server when you modify this file.
 
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
