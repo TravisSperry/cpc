7888f7a2daf5f0846e0dd0300c580e7ca567e531
Travis Sperry <travissperry1@gmail.com>
2019-11-27 15:44:13 -0500

    Add QR code to work order

---
+++ app/helpers/qr_code_helper.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module QRCodeHelper
+  def generate_qr(text)
+    barcode = RQRCode::QRCode.new(text, level: :q, size: 5)
+    qr_code = barcode.as_png(
+      bit_depth: 1,
+      border_modules: 4,
+      color: 'black',
+      file: nil,
+      fill: 'white',
+      module_px_size: 6,
+      resize_exactly_to: false,
+      resize_gte_to: false,
+      size: 400
+    )
+    ['data:image/png;base64,', qr_code.to_blob].pack('A*m').gsub(/\n/, '')
+  end
+end
+
