e771bb84a9cf40c9d9567a3f95b8e76555ae3c89
Travis Sperry <travissperry1@gmail.com>
2020-12-29 15:23:28 -0500

    safe navigation and userless ability check

--- app/ability/ability.rb
+++ app/ability/ability.rb
@@ -2,6 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    return unless user
     can :manage, :all if user.internal?
 
     can [:read, :update], User, customers_users: { customer: { id: user.customer_ids } }
