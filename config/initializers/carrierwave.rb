e2ceb6bb21ef5ab79f0ea4c8f2f1b050ab935f6d
Travis Sperry <tsperry@covermymeds.com>
2021-01-24 19:07:09 -0500

    update bundle bin and carrierwave testing config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,22 +1,43 @@
 # frozen_string_literal: true
 
-CarrierWave.configure do |config|
-  config.storage    = :aws
-  config.aws_bucket = ENV['CPC_S3_BUCKET']
-  config.aws_acl    = 'public-read'
+if Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
 
-  # The maximum period for authenticated_urls is only 7 days.
-  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+  CarrierWave::Uploader::Base.descendants.each do |klass|
+    next if klass.anonymous?
 
-  # Set custom options such as cache control to leverage browser caching
-  config.aws_attributes = {
-    expires: 1.week.from_now.httpdate,
-    cache_control: 'max-age=604800'
-  }
+    klass.class_eval do
+      def cache_dir
+        "#{Rails.root}/spec/support/uploads/tmp"
+      end
 
-  config.aws_credentials = {
-    access_key_id: ENV['CPC_S3_KEY'],
-    secret_access_key: ENV['CPC_S3_SECRET'],
-    region: 'us-east-1' # Required
-  }
+      def store_dir
+        "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      end
+    end
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage    = :aws
+    config.aws_bucket = ENV['CPC_S3_BUCKET']
+    config.aws_acl    = 'public-read'
+
+    # The maximum period for authenticated_urls is only 7 days.
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+
+    # Set custom options such as cache control to leverage browser caching
+    config.aws_attributes = {
+      expires: 1.week.from_now.httpdate,
+      cache_control: 'max-age=604800'
+    }
+
+    config.aws_credentials = {
+      access_key_id: ENV['CPC_S3_KEY'],
+      secret_access_key: ENV['CPC_S3_SECRET'],
+      region: 'us-east-1' # Required
+    }
+  end
 end
