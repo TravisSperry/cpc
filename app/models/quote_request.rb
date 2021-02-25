438da83e6e05a334f2fdbb80303da2ee3f9b57d5
Travis Sperry <tsperry@covermymeds.com>
2021-02-25 13:16:41 -0500

    add created at date

--- app/models/quote_request.rb
+++ app/models/quote_request.rb
@@ -16,10 +16,9 @@ class QuoteRequest < ApplicationRecord
 
   def self.to_csv
     CSV.generate do |csv|
-      csv << ['first name', 'last name', 'phone', 'email', 'job title', 'customer id', 'customer name', 'employee email',
-      'employee id', 'status', 'source']
+      csv << ['created at', 'first name', 'last name', 'phone', 'email', 'job title', 'customer id', 'customer name', 'employee email', 'employee id', 'status', 'source']
       all.each do |quote_request|
-        csv << [quote_request.first_name, quote_request.last_name, quote_request.telephone, quote_request.email, quote_request.job_title, quote_request.customer_id, quote_request.customer&.name, quote_request.user&.email, quote_request.user_id, quote_request.status, quote_request.source]
+        csv << [quote_request.created_at&.iso8601, quote_request.first_name, quote_request.last_name, quote_request.telephone, quote_request.email, quote_request.job_title, quote_request.customer_id, quote_request.customer&.name, quote_request.user&.email, quote_request.user_id, quote_request.status, quote_request.source]
       end
     end
   end
