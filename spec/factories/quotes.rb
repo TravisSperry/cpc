f43ba8ee4e594f53ef6862405e17c3739acb5c98
Travis Sperry <tsperry@covermymeds.com>
2021-06-21 08:18:31 -0400

    quoting specs WIP

--- spec/factories/quotes.rb
+++ spec/factories/quotes.rb
@@ -1,11 +1,17 @@
 FactoryBot.define do
   factory :quote do
     quote_request { nil }
-    labor_hours { "9.99" }
-    oven_hours { "9.99" }
-    powder_pounds { "9.99" }
-    cost_of_business { 1 }
-    pricing_scale { 1 }
+    labor_hours { rand(1.00..10.00).round(2) }
+    labor_rate { Cpc::QuoteForm::LABOR_RATE }
+    oven_hours { rand(1.00..10.00).round(2) }
+    hourly_oven_price { rand(0.01..1.00).round(2) }
+    powder_pounds { rand(1.00..10.00).round(2) }
+    powder_price { Cpc::QuoteForm::POWDER_PRICES.sample[1] }
+    cost_of_business { rand(0.01..1.00).round(2) }
+    pricing_scale { rand(0.01..1.00).round(2) }
+    sandblasting_rate { Cpc::QuoteForm::SANDBLASTING_RATE }
+    sandblasting_hours { rand(1.00..10.00).round(2) }
+    sandblasting_media_multiplier { Cpc::QuoteForm::SANDBLASTING_MEDIA_MULTIPLIER }
     user { nil }
   end
 end
